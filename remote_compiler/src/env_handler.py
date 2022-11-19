7c87ba050e3f1e3b0d292af2554a6124b1c631bf
Julien Bourque <julien.bourque@polymtl.ca>
2022-11-19 11:58:52 -0500

    Added build mutex

--- remote_compiler/src/env_handler.py
+++ remote_compiler/src/env_handler.py
@@ -3,8 +3,11 @@ import tempfile
 import shutil
 import os
 import subprocess
+import logging
 
-from threading import Thread, Lock
+from threading import Lock
+
+logger = logging.getLogger(__name__)
 
 
 class EnvHandler:
@@ -36,14 +39,18 @@ class EnvHandler:
         with self._mutex:
             self._directories[env_id] = env_path
 
+        logger.info("Adding new env: id: (%s), path: (%s)", env_id, env_path)
+
         return env_id
 
     def remove(self, env_id):
         if env_id not in self._directories:
-            raise Exception("Unknown id")
+            raise Exception(f"Unknown id: {env_id}")
 
         with self._mutex:
             env_path = self._directories.pop(env_id)
+
+        logger.info("Removing env_id: (%s)", env_id)
 
         shutil.rmtree(env_path)
 
@@ -52,11 +59,14 @@ class EnvHandler:
 
         with self._mutex:
             if env_id not in self._directories:
-                raise Exception("Unknown id")
+                raise Exception(f"Unknown id: {env_id}")
             base_path = os.path.join(self._directories.get(env_id), "src")
 
-        with open(os.path.join(base_path, path), mode='wb') as file:
+        file_path = os.path.join(base_path, path)
+        with open(file_path, mode='wb') as file:
             file.write(content)
+        
+        logger.info("From env_id (%s), editing file: (%s)", env_id, file_path)
 
     def get(self, env_id, path):
         path = os.path.normpath(path)
@@ -64,6 +74,7 @@ class EnvHandler:
         with self._mutex:
             if env_id not in self._directories:
                 base_path = self._source_path
+                logger.info("env_id (%s), not found in directory", env_id)
             else:
                 base_path = self._directories.get(env_id)
 
@@ -72,23 +83,29 @@ class EnvHandler:
         with open(os.path.join(base_path, path), mode='r') as file:
             data = file.read()
 
+        logger.info("From env_id (%s), retreiving file: (%s)", env_id, path)
+
         return data.encode('utf-8')
 
     def build(self, env_id, block_size: int):
         with self._mutex:
             if env_id not in self._directories:
-                raise Exception("Unknown id")
+                raise Exception(f"Unknown id: {env_id}")
             base_path = self._directories.get(env_id)
 
-        build_path = os.path.join(self._root_path, env_id)
-        shutil.copytree(base_path, build_path)
+            build_path = os.path.join(self._root_path, env_id)
+            shutil.copytree(base_path, build_path)
 
-        result = subprocess.run(
-            ["make"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=build_path)
+            logger.info("Received build request for env_id: %s", env_id)
+
+            result = subprocess.run(
+                ["make"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=build_path)
 
         if result.returncode != 0:
             shutil.rmtree(build_path)
-            raise Exception("Build error")
+            raise Exception(f"Build error, code: {result.returncode}")
+            
+        logger.info("Build succeded for env_id: %s", env_id)
 
         with open(os.path.join(build_path, "build", "cf2.bin"), mode='rb') as file:
             while data := file.read(block_size):
