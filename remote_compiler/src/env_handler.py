0747996c43aee03612f4e0929cb312c99ce26208
Julien Bourque <julien.bourque@polymtl.ca>
2022-11-19 11:32:05 -0500

    Added mutex to env handler

--- remote_compiler/src/env_handler.py
+++ remote_compiler/src/env_handler.py
@@ -4,12 +4,15 @@ import shutil
 import os
 import subprocess
 
+from threading import Thread, Lock
+
 
 class EnvHandler:
     _directories: dict
     _env_path: str
     _source_path: str
     _root_path: str
+    _mutex: Lock
 
     def __init__(self, app_path):
         self._directories = {}
@@ -17,6 +20,7 @@ class EnvHandler:
 
         self._source_path = os.path.join(self._root_path, "source")
         self._env_path = tempfile.mkdtemp()
+        self._mutex = Lock()
 
     def close(self):
         shutil.rmtree(self._env_path)
@@ -26,10 +30,11 @@ class EnvHandler:
         env_path = os.path.join(self._env_path, env_id)
 
         shutil.rmtree(os.path.join(self._source_path,
-                      "build"), ignore_errors=True)
+                    "build"), ignore_errors=True)
         shutil.copytree(self._source_path, env_path)
 
-        self._directories[env_id] = env_path
+        with self._mutex:
+            self._directories[env_id] = env_path
 
         return env_id
 
@@ -37,16 +42,18 @@ class EnvHandler:
         if env_id not in self._directories:
             raise Exception("Unknown id")
 
-        env_path = self._directories.pop(env_id)
+        with self._mutex:
+            env_path = self._directories.pop(env_id)
+
         shutil.rmtree(env_path)
 
     def edit(self, env_id, path, content):
-        if env_id not in self._directories:
-            raise Exception("Unknown id")
-
         path = os.path.normpath(path)
 
-        base_path = os.path.join(self._directories.get(env_id), "src")
+        with self._mutex:
+            if env_id not in self._directories:
+                raise Exception("Unknown id")
+            base_path = os.path.join(self._directories.get(env_id), "src")
 
         with open(os.path.join(base_path, path), mode='wb') as file:
             file.write(content)
@@ -54,10 +61,11 @@ class EnvHandler:
     def get(self, env_id, path):
         path = os.path.normpath(path)
 
-        if env_id not in self._directories:
-            base_path = self._source_path
-        else:
-            base_path = self._directories.get(env_id)
+        with self._mutex:
+            if env_id not in self._directories:
+                base_path = self._source_path
+            else:
+                base_path = self._directories.get(env_id)
 
         base_path = os.path.join(base_path, "src")
 
@@ -67,11 +75,13 @@ class EnvHandler:
         return data.encode('utf-8')
 
     def build(self, env_id, block_size: int):
-        if env_id not in self._directories:
-            raise Exception("Unknown id")
+        with self._mutex:
+            if env_id not in self._directories:
+                raise Exception("Unknown id")
+            base_path = self._directories.get(env_id)
 
         build_path = os.path.join(self._root_path, env_id)
-        shutil.copytree(self._directories.get(env_id), build_path)
+        shutil.copytree(base_path, build_path)
 
         result = subprocess.run(
             ["make"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=build_path)
